4753526effdad6977a552090224fba6bd2606531
Hunter Fehr <hunter.fehr3935@yahoo.com>
2022-12-04 14:58:15 -0800

    retest on lab

--- _tests_/server.test.js
+++ _tests_/server.test.js
@@ -1,22 +1,48 @@
+// const { app } = require('../server');
+// const supertest = require('supertest');
+// const request = supertest(app);
+
+// describe('APIServer', ()=>{
+//   it('handles roo path', async ()=>{
+//     const response = await request.get('/');
+
+//     expect(response.status).toBe(200);
+//     expect(response.text).toBeTruthy();
+//     expect (response.text).toEqual('Hello World')
+//   });
+
+//   it('handled invalid requests', async()=>{
+//     const response = await request.get('/foo');
+//     expect(response.status).toEqual(404);
+//   });
+
+//   it('handles errors', async ()=>{
+//     const response = await request.get('/bad');
+//     expect(response.status).toEqual(500);
+//     expect(response.body.route).toEqual('/bad');
+//   });
+// });
+
+
 const { app } = require('../server');
 const supertest = require('supertest');
 const request = supertest(app);
 
-describe('APIServer', ()=>{
-  it('handles roo path', async ()=>{
+describe('API Server', () => {
+  it('handles root path', async () => {
     const response = await request.get('/');
 
     expect(response.status).toBe(200);
     expect(response.text).toBeTruthy();
-    expect (response.text).toEqual('Hello World')
+    expect(response.text).toEqual('Hello World!!!');
   });
 
-  it('handled invalid requests', async()=>{
+  it('handles invalid requests', async () => {
     const response = await request.get('/foo');
     expect(response.status).toEqual(404);
   });
 
-  it('handles errors', async ()=>{
+  it('handles errors', async () => {
     const response = await request.get('/bad');
     expect(response.status).toEqual(500);
     expect(response.body.route).toEqual('/bad');
